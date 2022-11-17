35faf4b70e3dbc6758a271edd6a2421fa0d9f3b6
Ashish210901 <karunakaran.ashish@gmail.com>
2022-11-17 11:32:27 +0530

    Second tweak

--- sample.py
+++ sample.py
@@ -1,10 +1,10 @@
-from flask import Flask 
+from flask import Flask,render_template,request
 
 app=Flask(__name__)
 
 @app.route('/')
-def index():
-	return 'Hello World'
+def login():
+	return render_template('hpme.html')
 
 if __name__=='__main__':
 	app.run(debug=True)
